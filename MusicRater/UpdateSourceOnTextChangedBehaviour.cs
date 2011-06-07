61190f6233e0a361b9e5b475b11474434010c2e9
Mark Heath <mark.heath@gmail.com>
2011-06-07 22:12:44 +0100

    Added an update source on text changed behaviour for the textboxes so comments are not lost when navigating away

---
+++ MusicRater/UpdateSourceOnTextChangedBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+
+namespace MusicRater
+{
+    /// <summary>
+    /// this behaviour from http://jacob4u2.posterous.com/silverlight-4-textbox-update-source-on-text-c
+    /// </summary>
+    public class UpdateSourceOnTextChanged : Behavior<TextBox>
+    {
+        BindingExpression textBinding;
+
+        protected override void OnAttached()
+        {
+            if (this.AssociatedObject == null)
+                return;
+
+            // Get the binding
+            textBinding = this.AssociatedObject.GetBindingExpression(TextBox.TextProperty);
+
+            // Subscribe to text changed events.
+            this.AssociatedObject.TextChanged += AssociatedObject_TextChanged;
+
+            base.OnAttached();
+        }
+
+        void AssociatedObject_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Update the binding source.
+            if (textBinding != null)
+                textBinding.UpdateSource();
+        }
+
+        protected override void OnDetaching()
+        {
+            if (this.AssociatedObject == null)
+                return;
+
+            // Clean up...
+            this.textBinding = null;
+            this.AssociatedObject.TextChanged -= AssociatedObject_TextChanged;
+
+            base.OnDetaching();
+        }
+    }
+
+
+}
