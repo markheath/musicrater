85633129e5941f272643a579ccf5f13086ee3f84
Mark Heath <mark.heath@gmail.com>
2011-06-08 14:08:41 +0100

    Added a unit test assembly based on the Silverlight toolkit

---
+++ MusicRater.Tests/RatingsRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MusicRater.Tests
+{
+    [TestClass]
+    public class RatingsRepositoryTests
+    {
+        [TestMethod]
+        public void NoExceptionIfNotFound()
+        {
+            RatingsRepository rr = new RatingsRepository();
+            var r = rr.Load();
+            Assert.AreEqual(0, r.Count());
+        }
+    }
+}
