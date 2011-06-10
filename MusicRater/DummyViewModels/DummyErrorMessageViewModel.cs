02994e99082cf4107ac5ad92928cb5be466d1f92
Mark Heath <mark.heath@gmail.com>
2011-06-10 17:44:54 +0100

    Added unity and beginnings of a framework for modal dialogs

---
+++ MusicRater/DummyViewModels/DummyErrorMessageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MusicRater.DummyViewModels
+{
+    public class DummyErrorMessageViewModel
+    {
+        public string Message
+        {
+            get
+            {
+                return "Ham hock ball tip deserunt ad non corned beef. Hamburger tail aute beef ribs. Et ut enim eiusmod. Consectetur aute in, pork belly short loin consequat ground round. Consequat t-bone non tongue. Proident minim magna, aliquip nisi tongue est. Enim pork exercitation shank tempor ut.";
+            }
+        }
+    }
+}
---
+++ MusicRater/ModalDialogService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+// This approach to modal dialogs in Silverlight with MVVM from
+// http://blog.roboblob.com/2010/01/19/modal-dialogs-with-mvvm-and-silverlight-4/
+
+namespace MusicRater
+{
+    
+    public interface IModalWindow
+    {
+        bool? DialogResult { get; set; }
+        event EventHandler Closed;
+        void Show();
+        object DataContext { get; set; }
+        void Close();
+    }
+
+    public interface IModalDialogService
+    {
+        void ShowDialog<TViewModel>(IModalWindow view, TViewModel viewModel, Action<TViewModel> onDialogClose);
+
+        void ShowDialog<TDialogViewModel>(IModalWindow view, TDialogViewModel viewModel);
+    }
+
+    public class ModalDialogService : IModalDialogService
+    {
+        public void ShowDialog<TDialogViewModel>(IModalWindow view, TDialogViewModel viewModel, Action<TDialogViewModel> onDialogClose)
+        {
+            view.DataContext = viewModel;
+            if (onDialogClose != null)
+            {
+                view.Closed += (sender, e) => onDialogClose(viewModel);
+            }
+            view.Show();
+        }
+
+        public void ShowDialog<TDialogViewModel>(IModalWindow view, TDialogViewModel viewModel)
+        {
+            this.ShowDialog(view, viewModel, null);
+        }
+    }
+
+}
