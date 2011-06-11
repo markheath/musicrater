f194ba92934218e2cc35c4f5544e1707f9d05da8
Mark Heath <mark.heath@gmail.com>
2011-06-11 13:52:46 +0100

    Styled the download progress bar

---
+++ MusicRater/DummyViewModels/DummyPositionControlViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MusicRater.DummyViewModels
+{
+    public class DummyPositionControlViewModel
+    {
+        public double DownloadProgress { get { return 55; } }
+        public double Duration { get { return 265; } }
+        public double PlaybackPosition { get { return 101; } }
+    }
+}
