539d38e4c1644c8115e363c0d0d3f2a1c8c8667a
Mark Heath <mark.heath@gmail.com>
2011-06-21 13:11:22 +0100

    Adding a configuration window (not currently used)

---
+++ MusicRater/DummyViewModels/DummyConfigWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace MusicRater.DummyViewModels
+{
+    public class DummyConfigWindowViewModel
+    {
+        public DummyConfigWindowViewModel()
+        {
+            this.Criteria = new List<Criteria>();
+            this.Criteria.Add(new Criteria("Something", 6));
+            this.Criteria.Add(new Criteria("Something Else", 10));
+            this.Criteria.Add(new Criteria("A Third Criteria", 2));
+            this.Criteria.Add(new Criteria("Unused 1", 0));
+            this.Criteria.Add(new Criteria("Unused 2", 2));
+        }
+
+        public List<Criteria> Criteria { get; private set; }
+    }
+}
