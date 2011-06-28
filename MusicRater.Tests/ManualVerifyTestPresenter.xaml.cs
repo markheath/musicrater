94b3444ee21f8ce6c165a3ddc8969f7559bb6fcc
Mark Heath <mark.heath@gmail.com>
2011-06-28 14:10:59 +0100

    adding manual verify unit test capabilities

---
+++ MusicRater.Tests/ManualVerifyTestPresenter.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Silverlight.Testing;
+
+namespace MusicRater.Tests
+{
+    public partial class ManualVerifyTestPresenter : UserControl
+    {
+        private WorkItemTest test;
+
+        public ManualVerifyTestPresenter()
+        {
+            InitializeComponent();                
+        }
+
+        public void Init(string description, object content, WorkItemTest test)
+        {
+            this.textDescription.Text = description;
+            this.testContent.Content = content;
+            this.test = test;
+        }
+
+        private void Pass_Click(object sender, RoutedEventArgs e)
+        {
+            this.test.EnqueueTestComplete();
+        }
+
+        private void Fail_Click(object sender, RoutedEventArgs e)
+        {            
+            Assert.Fail();
+        }
+
+        private void Skip_Click(object sender, RoutedEventArgs e)
+        {
+            Assert.Inconclusive();
+        }
+
+    }
+}
--- MusicRater.Tests/ToolbarControlTests.cs
+++ MusicRater.Tests/ToolbarControlTests.cs
@@ -34,6 +34,15 @@ namespace MusicRater.Tests
             Assert.IsTrue(tc.ActualWidth > 0);
         }
 
+        /*
+        [TestMethod]
+        public void ClickPlay()
+        {
+            Button b; //tc.Cont
+            
+            Assert.IsTrue(tc.ActualWidth > 0);
+        }*/
+
         [TestMethod]
         [Asynchronous]
         public void WaitForABit()
@@ -41,10 +50,41 @@ namespace MusicRater.Tests
             // need to work out how to make it interactive here
             //this.EnqueueDelay(5000);
             viewModel.PlayCommand.Executed += (sender, args) => this.TestComplete();
+        }        
+    }
+
+    [TestClass]
+    public class ToolbarControlManualVerifyTests : SilverlightTest
+    {
+        private ManualVerifyTestPresenter presenter;
+        private TestToolbarControlViewModel viewModel;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            this.presenter = new ManualVerifyTestPresenter();
+            this.TestPanel.Children.Add(presenter);
         }
 
+        [TestMethod]
+        [Asynchronous]
+        public void CheckToolTips()
+        {
+            this.presenter.Init("Confirm that each button has the correct tooltip.",
+                new ToolbarControl(), this);
+        }
         
+        [TestMethod]
+        [Asynchronous]
+        public void CheckShuffledList()
+        {
+            var lb = new ListBox();
+            lb.ItemsSource = new [] {1,2,3,4,5,6,7,8,9,10,11,12};
+            this.presenter.Init("Confirm that these numbers are in random order.",
+                lb, this);
+        }
     }
+
     public class TestToolbarControlViewModel
     {
         public TestableCommand PlayCommand { get; private set; }
